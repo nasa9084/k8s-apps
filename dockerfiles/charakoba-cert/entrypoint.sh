5b5ec5c6ad314e49bab138c2c818cfd69f9adeac
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2018-08-24 10:29:35 +0900

    update script to update certs

--- dockerfiles/charakoba-cert/entrypoint.sh
+++ dockerfiles/charakoba-cert/entrypoint.sh
@@ -2,4 +2,4 @@
 
 mc config host add minio http://192.168.1.54:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 mc cp minio/charakoba-cert/fullchain.pem minio/charakoba-cert/privkey.pem .
-kubectl create secret tls rm.charakoba.com --key privkey.pem --cert fullchain.pem
+kubectl create secret tls rm.charakoba.com --key privkey.pem --cert fullchain.pem --dry-run -o yaml | kubectl replace -f-
