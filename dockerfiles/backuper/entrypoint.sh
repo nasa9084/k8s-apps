eb1e9306c7a74d71b1e07eda2292192b92cea059
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2018-08-31 11:19:56 +0900

    remove old backup for ghost

--- dockerfiles/backuper/entrypoint.sh
+++ dockerfiles/backuper/entrypoint.sh
@@ -8,3 +8,4 @@ FILENAME=backup-${BACKUP_TARGET}-$(date +%Y%m%d%H%M%S).tar.gz
 tar -zcvf /tmp/${FILENAME} /target
 mc config host add minio http://192.168.1.54:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 mc cp /tmp/${FILENAME} minio/backups/${BACKUP_TARGET}/${FILENAME}
+mc rm -r --older-than 6 --force minio/backups/ghost/
