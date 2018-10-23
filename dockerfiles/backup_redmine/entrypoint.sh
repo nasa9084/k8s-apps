aa2de2df6d1de3fa05146c96b48119f294ad9d69
nasa9084 <nasa.9084.bassclarinet@gmail.com>
2018-10-23 17:26:21 +0900

    use new minio cluster

--- dockerfiles/backup_redmine/entrypoint.sh
+++ dockerfiles/backup_redmine/entrypoint.sh
@@ -11,6 +11,6 @@ chmod 0600 ~/.pgpass
 pg_dump -U ${PGUSER} -Fc --file=/tmp/redmine/redmine.sqlc ${PGDATABASE} -w
 
 tar -zcvf /tmp/${FILENAME} /tmp/redmine
-mc config host add minio http://192.168.1.54:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
+mc config host add minio http://192.168.1.56:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 mc cp /tmp/${FILENAME} minio/backups/redmine/${FILENAME}
 mc rm -r --older-than 6 --force minio/backups/redmine/
